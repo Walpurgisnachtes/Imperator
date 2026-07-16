88a6b51ac2548ca7f03c1192d6a0684fa38590b2
Matthew <tatoba2351@gmail.com>
2026-07-16 16:57:24 +0800

    chore: add lingui for translation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
+import { lingui } from "@lingui/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tailwindcss(),
+    babel({
+      presets: [reactCompilerPreset()],
+      plugins: ["@lingui/babel-plugin-lingui-macro"],
+    }),
+    lingui(),
   ],
-})
+});
