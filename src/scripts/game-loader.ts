7e23dab0cfc2112d8f8c887433379e9edda97246
Matthew <tatoba2351@gmail.com>
2026-07-19 00:40:22 +0800

    feat: game now created w/ new game status and 1 capital

--- src/scripts/game-loader.ts
+++ src/scripts/game-loader.ts
@@ -1,24 +1,35 @@
 import { createNewCityData } from "../types/city-data";
-import { gameStatus as gameData } from "../data/static-data/game-data";
+import {
+  gameStatus as gameData,
+  resetGameStatus,
+} from "../data/static-data/game-data";
 import type { GameStatus } from "../types/game-status";
+import { createCityInfo } from "../types/city-info";
+import { getCapitalCity, setCapitalCity } from "../data/capital-city";
+import { CityInfoStore } from "../types/city-info-store";
 
 // Use async prepare for future large loading
 export async function createNewGame(): Promise<void> {
-  gameData.cities.push(createNewCityData([]));
+  resetGameStatus();
+
+  const newCity = createNewCityData([]);
+  gameData.cities.push(newCity);
+  setCapitalCity(newCity.id);
+
+  CityInfoStore.setCityInfo(createCityInfo(getCapitalCity()));
 }
 
 export async function loadGameData(file: string): Promise<void> {
   await JSON.parse(file, (key, value) => {
     if (key === "gameStatus") {
       const loadedGameData: GameStatus = value;
-      window.dispatchEvent(
-        new CustomEvent("game-status-updated", { detail: loadedGameData }),
-      );
 
       gameData.hash = loadedGameData.hash;
       gameData.gameVersion = loadedGameData.gameVersion;
       gameData.cities = loadedGameData.cities;
       gameData.capitalId = loadedGameData.capitalId;
+
+      CityInfoStore.setCityInfo(createCityInfo(getCapitalCity()));
     }
   });
 }
