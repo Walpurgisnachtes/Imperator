d66d6d04b696852f441e31f737c2eb03f579f353
Matthew <tatoba2351@gmail.com>
2026-07-18 22:29:52 +0800

    chore: create city data context

---
+++ src/types/city-data-context.ts
@@ -0,0 +1,27 @@
+import type { CityData } from "./city-data";
+import {createContext} from "react";
+
+export const CityDataContext = createContext<CityData>(
+  {
+    id: "",
+    resources: {},
+    buildings: [],
+    name: "",
+    population: 0,
+    treasury: 0,
+    happiness: 0,
+    stability: 0,
+    foodSupply: 0,
+    productionStrength: 0,
+    tradeStrength: 0,
+    militaryStrength: 0,
+    technologyStrength: 0,
+    cultureStrength: 0,
+    politicalStrength: 0,
+    religionStrength: 0,
+    crimeStrength: 0,
+    animosityStrength: 0,
+    animosityDirection: "none",
+    warWearinessStrength: 0,
+  }
+);
