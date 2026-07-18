63cab5a91f775c0606930171b7a1dcc80b15eac8
Matthew <tatoba2351@gmail.com>
2026-07-19 00:41:17 +0800

    chore: basic building list

--- src/components/City/BuildingsList.tsx
+++ src/components/City/BuildingsList.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import type { CityData } from "../../types/city-data";
-import { gameRules } from "../../data/static-data/game-rules";
+import { getGameRuleValueById } from "../../data/static-data/game-rules";
 
+// TODO: Remove the magic number 10000
 export const BuildingsList: React.FC<{ data: CityData }> = ({ data }) => {
-  const buildingCapacity = gameRules.buildingCapacityPerCity +
-    Math.floor(data.population / 100000) * gameRules.buildingCapacityIncrement || 0;
+  const buildingCapacity =
+    (getGameRuleValueById<number>("buildingCapacityPerCity") || 0) +
+    Math.floor(data.population / 10000) *
+      (getGameRuleValueById<number>("buildingCapacityIncrement") || 0);
 
+  const canBuildMore = data.buildings.length < buildingCapacity;
 
   return (
-    <div id="buildings-list" className="relative flex flex-col gap-4 w-2xs max-h-[80vh] rounded-xl border border-slate-700/50 bg-slate-800/40 p-4 backdrop-blur-sm">
+    <div
+      id="buildings-list"
+      className="relative flex flex-col gap-4 w-3xl max-h-[80vh] rounded-xl border border-slate-700/50 bg-slate-800/40 p-4 backdrop-blur-sm"
+    >
       <ul>
-        {data.buildings.map((building) => (
-          <li key={building.id}>
+        {data.buildings.map((building, idx) => (
+          <li
+            key={building.id}
+            className={`text-lg font-bold text-slate-100 ${idx < data.buildings.length - 1 || canBuildMore ? `border-b border-slate-700 pb-2` : ``}`}
+          >
             {building.name} - Level: {building.level}
           </li>
         ))}
+        {canBuildMore && (
+          <li className="text-lg font-bold text-slate-100">
+            Build more spaceholder
+          </li>
+        )}
       </ul>
     </div>
   );
