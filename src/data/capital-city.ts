ff3b62125666b17c7d4b3c4f9d9520b3305d2956
Matthew <tatoba2351@gmail.com>
2026-07-19 00:38:16 +0800

    feat: provide manager to find capital city

---
+++ src/data/capital-city.ts
@@ -0,0 +1,23 @@
+import { gameStatus } from "./static-data/game-data";
+import type { CityData } from "../types/city-data";
+import { systemMsg } from "./static-data/system-msg";
+import { i18n } from "@lingui/core";
+
+export function getCapitalCity(): CityData {
+  let capitalCity = gameStatus.cities.find(
+    (city) => city.id === gameStatus.capitalId,
+  );
+  if (capitalCity) {
+    return capitalCity;
+  } else {
+    console.error(i18n.t(systemMsg.capitalCityNotFound), gameStatus.cities);
+    return gameStatus.cities[0];
+  }
+}
+
+export function setCapitalCity(cityId: string): void {
+  const city = gameStatus.cities.find((city) => city.id === cityId);
+  if (city) {
+    gameStatus.capitalId = cityId;
+  }
+}
