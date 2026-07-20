bc8cf1203ee63bf2db732b196fc4e03680529b0f
Matthew <tatoba2351@gmail.com>
2026-07-20 16:59:36 +0800

    chore: add 2 new buildings

---
+++ src/data/static-data/buildings/hunting_lodge.ts
@@ -0,0 +1,43 @@
+import { registerBuilding, createDefaultBuilding } from "../buildings";
+import type { ProductionRecipe } from "../../../types/production-recipe";
+import type { BuildingRequirement } from "../../../types/building-requirement";
+import type { MessageDescriptor } from "@lingui/core";
+import { msg } from "@lingui/core/macro";
+
+let buildingName: MessageDescriptor = msg`Hunting Lodge`;
+let buildingDescription: MessageDescriptor = msg`Provides furs from forests.\nFurs are used for clothing and luxury trade.`;
+let buildingProduction: ProductionRecipe[] = [
+    {
+      inputs: {},
+      outputs: {
+        furs: 10,
+      },
+      productionStrengthCost: 10,
+    }
+  ];
+let buildingTags: string[] = ["forestry"];
+let buildingRequirements: BuildingRequirement = {
+  allows: {
+    furs: 1,
+  },
+};
+let buildingCost: number = 0;
+let buildingMaintenance: number = 0;
+let buildingMaxHp: number = 100;
+let buildingProductionStrengthCostMultiplier: number = 1;
+let buildingProductionOutputMultiplier: number = 1;
+
+registerBuilding(
+  createDefaultBuilding(
+    buildingName,
+    buildingDescription,
+    buildingProduction,
+    buildingTags,
+    buildingRequirements,
+    buildingCost,
+    buildingMaintenance,
+    buildingMaxHp,
+    buildingProductionStrengthCostMultiplier,
+    buildingProductionOutputMultiplier,
+  ),
+);
