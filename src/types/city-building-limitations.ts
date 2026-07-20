e4081f3e4ae25dea3d3c78c68285c2c0709fb250
Matthew <tatoba2351@gmail.com>
2026-07-20 20:23:46 +0800

    chore: change city building limitations name to default city data

--- src/types/city-building-limitations.ts
+++
@@ -1,8 +0,0 @@
-import type { MessageDescriptor } from "@lingui/core";
-import type { GeometryLimitations } from "./geometry-limitations";
-
-export type CityBuildingLimitation = {
-  name: MessageDescriptor;
-  buildingLimit: number;
-  geometryLimitation: GeometryLimitations;
-};
