053a4a619174eee23d2bf6cae5a41b7257409f79
Matthew <tatoba2351@gmail.com>
2026-07-22 17:58:55 +0800

    feat: add city action column

---
+++ src/components/City/CityActions/CityActionButton.tsx
@@ -0,0 +1,47 @@
+import type { FC, SVGProps } from "react";
+import { createElement } from "react";
+import { i18n, type MessageDescriptor } from "@lingui/core";
+import {
+  GameContentHTMLContextStore,
+  type GameContentHTMLContext,
+} from "../../../types/game-content-html-context-store";
+
+interface CityActionButtonProps {
+  imageComponent: FC<SVGProps<SVGSVGElement>>;
+  actionName: GameContentHTMLContext;
+  actionTitle: MessageDescriptor;
+  currentSelectedAction: GameContentHTMLContext;
+  onClick: () => void;
+  occupiesFullWidth?: boolean;
+  imageSize?: number;
+}
+
+export const CityActionButton: FC<CityActionButtonProps> = ({
+  imageComponent,
+  actionName,
+  actionTitle,
+  currentSelectedAction,
+  onClick,
+  occupiesFullWidth,
+  imageSize = 16,
+}) => {
+  return (
+    <button
+      className={`${
+        occupiesFullWidth ? "col-span-2" : ""
+      } w-24 h-24 p-2 m-2 flex flex-col justify-center items-center shadow-md shadow-slate-900/10 hover:shadow-slate-900/20 border-4 rounded-xl border-gray-800 disabled:border-amber-600 hover:not-disabled:brightness-150 transition-all duration-200`}
+      onClick={() => {
+        onClick();
+        GameContentHTMLContextStore.setGameContentHTMLContext(actionName);
+      }}
+      disabled={currentSelectedAction === actionName}
+    >
+      {createElement(imageComponent, {
+        className: `w-${imageSize} h-${imageSize}`,
+      })}
+      <span className="text-sm font-medium text-slate-200">
+        {i18n.t(actionTitle)}
+      </span>
+    </button>
+  );
+};
---
+++ src/components/City/CityActions/CityActionSelector.tsx
@@ -0,0 +1,25 @@
+import { useState, type FC } from "react";
+import { CityActionButton } from "./CityActionButton";
+import type { GameContentHTMLContext } from "../../../types/game-content-html-context-store";
+import { cityActions } from "../../../types/city-action";
+
+export const CityActionSelector: FC = () => {
+  const [selectedAction, setSelectedAction] =
+    useState<GameContentHTMLContext>("city/building");
+
+  return (
+    <div className="grid grid-cols-2 content-start justify-items-center gap-4 w-2xs max-h-[80vh] rounded-xl border border-slate-700/50 bg-slate-800/40 p-4 backdrop-blur-sm">
+      {cityActions.map((action, idx) => (
+        <CityActionButton
+          key={action.actionName}
+          imageComponent={action.imageComponent}
+          actionName={action.actionName}
+          actionTitle={action.actionTitle}
+          currentSelectedAction={selectedAction}
+          onClick={() => setSelectedAction(action.actionName)}
+          occupiesFullWidth={idx === cityActions.length - 1}
+        />
+      ))}
+    </div>
+  );
+};
